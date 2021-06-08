309620644b92e1d339816331dae4d1d36a85ce6f
Mohitgola0076 <75782323+Mohitgola0076@users.noreply.github.com>
2021-06-09 01:01:52 +0530

    indexing

---
+++ Indexing_sequence.py
@@ -0,0 +1,29 @@
+# 1.)  print index of string list
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+print(x)
+    #  It will print the index where word is present
+
+# 2.)  print index of int list 
+numbers = [4, 55, 64, 32, 16, 32]
+x = numbers.index(32)
+print(numbers)
+     #  It will print the index where number is present
+     
+# 3.)  Print element with index
+print(numbers[3])
+print(numbers[5])
+     # It will print the number present at the given index
+     
+# 4.)  Print values in given range 
+print(numbers[0:5]         # It will print the value from index 0 to 5.
+print(numbers[0:]           #It will print whole list from indexing 0 to end
+
+# 5.)  Printing from negatuve index
+print(numbers[:-1]        # It will print elements from the end to begning 
+
+# 6.)  Print values after jumping given index
+print(numbers[0,6,1])
+
+
+     
